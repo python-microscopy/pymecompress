519fc442ada20f43b481a561be462d0ce1fd820c
David Baddeley <david.baddeley.nz@gmail.com>
2016-07-26 17:29:36 -0400

    more changes for compression relocation and building with mingw

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ else:
 
 #windows VC++ has really shocking c standard support so we need to include
 #custom stdint.h and intypes.h files from https://code.google.com/archive/p/msinttypes
-print os.environ.get('CC', 'foo')
-if sys.platform == 'win32' and not os.environ.get('CC', '') == 'mingw':
+#print os.environ.get('CC', 'foo')
+if False: #sys.platform == 'win32' and not os.environ.get('CC', '') == 'mingw':
 	extra_include_dirs = ['win_incl']
 else:
 	extra_include_dirs = []
@@ -76,7 +76,7 @@ numpy.distutils.mingw32ccompiler.Mingw32CCompiler.link = link
 
 def configuration(parent_package = '', top_path = None):
     from numpy.distutils.misc_util import Configuration, get_numpy_include_dirs
-    config = Configuration('bcl', parent_package, top_path)
+    config = Configuration('pymecompress', parent_package, top_path)
 
     config.add_extension('bcl',
         sources=['bcl.pyx', 'src/huffman.c', 'quantize.c'],
