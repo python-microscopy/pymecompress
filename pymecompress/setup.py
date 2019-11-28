c1c57f0efcd3aebbe153b81c31e6abe9a1f81403
David Baddeley <david.baddeley.nz@gmail.com>
2019-11-29 10:10:14 +1300

    restructuring to better reflect what is BCL and what is us

--- pymecompress/setup.py
+++ pymecompress/setup.py
@@ -113,8 +113,8 @@ def configuration(parent_package = '', top_path = None):
                         
     
     ext = Extension(name=name,
-                    sources=[os.path.join(cur_dir, 'bcl.pyx'), os.path.join(cur_dir, 'src/huffman.c'), os.path.join(cur_dir, 'quantize.c')],
-                    include_dirs=['src',] + get_numpy_include_dirs() + extra_include_dirs,
+                    sources=[os.path.join(cur_dir, 'bcl.pyx'), os.path.join(cur_dir, 'bcl/huffman.c'), os.path.join(cur_dir, 'quantize.c')],
+                    include_dirs=['bcl',] + get_numpy_include_dirs() + extra_include_dirs,
                     extra_compile_args=['-O3', '-fno-exceptions', '-ffast-math', '-march=native', '-mtune=native'],
                     extra_link_args=linkArgs)
 
