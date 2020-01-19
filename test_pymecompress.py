f2da842012f6a8e257238abf1ac02175ae15f5d6
barentine <andrew.barentine@gmail.com>
2020-01-19 10:27:10 -0500

    moving loss-within-sigma test and removing PYME imports from it

--- test_pymecompress.py
+++ test_pymecompress.py
@@ -17,3 +17,19 @@ def test_compression_lossless_uint8():
                                    test_data.nbytes).view(test_data.dtype).reshape(test_data.shape)
 
     assert np.allclose(result, test_data)
+
+def test_loss_within_sigma():
+    from pymecompress import bcl
+    offset = 100
+    scale = 1
+    ground = np.ascontiguousarray(np.arange(offset, offset + 100, 1).reshape(10, 10).astype('uint16'))
+
+    quantized = bcl.HuffmanDecompress(bcl.HuffmanCompressQuant(np.frombuffer(ground.data, dtype='uint16'), offset,
+                                                               scale),
+                                      int(ground.nbytes / 2)).reshape(ground.shape)
+    quantized = quantized.astype(float) ** 2 + offset
+
+    assert np.all(
+        np.less_equal(np.abs(ground.astype(float) - quantized.squeeze().astype(float)),
+                      np.sqrt(np.sqrt(scale) * (ground.astype(float) - float(offset))))
+    )
